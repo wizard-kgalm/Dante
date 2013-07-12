c4cb03f52f05e4c9ba2a86a0bbc4399b05f5d769
wizard-kgalm <cameron43@comcast.net>
2013-07-12 14:49:02 -0700

    Update forge.php

--- scripts/forge.php
+++ scripts/forge.php
@@ -3,28 +3,24 @@
 // Name: Forge
 // Description: Forge a "quoted" message.
 // Author: Wizard-Kgalm
-// Version: 0.1
+// Version: 1.2
 // Priv: 0
-// Help: forge [action/username] [message]. If action, forge [action] [username] [message]. 
+// Help: {tr}forge [action/username] [message]. If action, {tr}forge [action] [username] [message]. 
 // ====================
 if( empty( $args[1] ) ){
-  return $dAmn->say( "{$from}: You must provide someone to forge messages from.", $c );
+	return $dAmn->say( "$from: You must provide someone to forge messages from.", $c );
 }
 $dateobj = date_create( "now", timezone_open( date_default_timezone_get() ) );
-if( $args[1] == "action" ){
-	if( $args[2][0] == "#" ){
-		$todochat = $args[2];
-		return $dAmn->say( date_format( $dateobj, "g:i:sA" )." <i><b>* {$args[3]}</b> $argsE[4] </i>", $todochat );
-	}else{
-		$todochat = $c;
-	}
-	return $dAmn->say( date_format( $dateobj, "g:i:sA" )." <i><b>* {$args[2]}</b> $argsE[3] </i>", $todochat );
+$say = date_format( $dateobj, "g:i:sA" )." ";
+if(  $args[1] == "action" ){
+	($args[2] == rChatName( $args[2] ) ) ? $todochat = $args[2]  : $todochat = $c;
+	($args[2] == rChatName( $args[2] ) ) ? $msg      = $argsE[4] : $msg      = $argsE[3];
+	($args[2] == rChatName( $args[2] ) ) ? $un       = $args[3]  : $un       = $args[2];
+} else {
+	($args[1] == rChatName( $args[1] ) ) ? $todochat = $args[1]  : $todochat = $c;
+	($args[1] == rChatName( $args[1] ) ) ? $msg      = $argsE[3] : $msg      = $argsE[2];
+	($args[1] == rChatName( $args[1] ) ) ? $un       = $args[2]  : $un       = $args[1];
 }
-if( $args[1][0] =='#' ){
-	$todochat = $args[1];
-	return $dAmn->say( date_format( $dateobj, "g:i:sA" )." <b><{$args[2]}></b> $argsE[3]", $todochat );
-}
-$todochat = $c;
-$dAmn->say( date_format( $dateobj, "g:i:sA" )." <b><{$args[1]}></b> $argsE[2]", $todochat );
-unset( $todochat );
-return;
+( strtolower( $args[1] ) == "action" ) ? $say .= "<i><b>* {$un}</b> $msg </i>" : $say .= "<b><{$un}></b> $msg";
+$dAmn->say( $say, $todochat );
+?>
