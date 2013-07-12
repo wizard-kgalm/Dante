5e336b545fd03e3c1e3464a33c6e1fff410297cb
wizard-kgalm <cameron43@comcast.net>
2013-07-12 14:57:30 -0700

    Create mad.php

---
+++ scripts/mad.php
@@ -0,0 +1,10 @@
+<?php
+// DANTE SCRIPT ======
+// Name: Mad
+// Description: Some nonsense icon set.
+// Author: Wizard-Kgalm 
+// Version: 0.1
+// Priv: default
+// Help: {tr}mad.
+// ====================
+$dAmn->say( base64_decode( "Omljb251bWFkMXBsejo6aWNvbnVtYWQycGx6OjppY29udW1hZDNwbHo6Cgk6aWNvbnVtYWQ0cGx6OjppY29udW1hZDVwbHo6Omljb251bWFkNnBsejo=" ), $c );
