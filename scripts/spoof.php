c5e614ead4b0636ad5ec6fc8d5dee905b6809d65
wizard-kgalm <cameron43@comcast.net>
2013-07-12 15:53:49 -0700

    Create spoof.php

---
+++ scripts/spoof.php
@@ -0,0 +1,10 @@
+<?php
+// DANTE SCRIPT ======
+// Name: Spoof
+// Description: Prints out the current browser the bot is spoofing. 
+// Author: SubjectX52873M
+// Version: 1.0
+// Help: "No options"
+// ====================
+$dAmn->say( "Spoofing: ".BotHttpUserAgent, $c );
+?>
