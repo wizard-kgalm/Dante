9ac67b924fcd020ac6bb4693fb83819561a1dde0
wizard-kgalm <cameron43@comcast.net>
2013-07-12 15:22:43 -0700

    Create smile.php

---
+++ scripts/smile.php
@@ -0,0 +1,11 @@
+<?php
+// DANTE SCRIPT ======
+// Name: Negrosmile
+// Description: Smile like a negro.
+// Author: Wizard-Kgalm 
+// Version: 1.0
+// Priv: default
+// Help:  Just {tr}smile.
+// ====================
+$dAmn->say( base64_decode("Omljb25zbWlsaW5nbmVncm8xcGx6OjppY29uc21pbGluZ25lZ3JvMnBsejo6aWNvbnNtaWxpbmduZWdybzNwbHo6Cgk6aWNvbnNtaWxpbmduZWdybzRwbHo6Omljb25zbWlsaW5nbmVncm81cGx6OjppY29uc21pbGluZ25lZ3JvNnBsejo="),$c );
+?>
