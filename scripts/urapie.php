491bc514bbc3d5e44dbcdb04f1cc6aaff72e102d
wizard-kgalm <cameron43@comcast.net>
2013-07-12 16:26:43 -0700

    Create urapie.php

---
+++ scripts/urapie.php
@@ -0,0 +1,10 @@
+<?php
+// DANTE SCRIPT ======
+// Name: Urapie
+// Description: NO, UR A PIE. Some Cheesy icon set.
+// Author: Wizard-Kgalm
+// Version: 1.0
+// Priv: 0
+// Help: {tr}urapie
+// ====================
+$dAmn->say( @base64_decode("Omljb25ub3VyYXBpZTFwbHo6Omljb25ub3VyYXBpZTJwbHo6Omljb25ub3VyYXBpZTNwbHo6Omljb25ub3VyYXBpZTRwbHo6Cgk6aWNvbm5vdXJhcGllNXBsejo6aWNvbm5vdXJhcGllNnBsejo6aWNvbm5vdXJhcGllN3Bsejo6aWNvbm5vdXJhcGllOHBsejoKCTppY29ubm91cmFwaWU5cGx6OjppY29ubm91cmFwaWUxMHBsejo6aWNvbm5vdXJhcGllMTFwbHo6Omljb25ub3VyYXBpZTEycGx6OgoJOmljb25ub3VyYXBpZTEzcGx6OjppY29ubm91cmFwaWUxNHBsejo6aWNvbm5vdXJhcGllMTVwbHo6Omljb25ub3VyYXBpZTE2cGx6OgoJOmljb25ub3VyYXBpZTE3cGx6OjppY29ubm91cmFwaWUxOHBsejo6aWNvbm5vdXJhcGllMTlwbHo6Omljb25ub3VyYXBpZTIwcGx6OgoJOmljb25ub3VyYXBpZTIxcGx6OjppY29ubm91cmFwaWUyMnBsejo6aWNvbm5vdXJhcGllMjNwbHo6Omljb25ub3VyYXBpZTI0cGx6OgoJOmljb25ub3VyYXBpZTI1cGx6OjppY29ubm91cmFwaWUyNnBsejo6aWNvbm5vdXJhcGllMjdwbHo6Omljb25ub3VyYXBpZTI4cGx6Og=="), $c );
