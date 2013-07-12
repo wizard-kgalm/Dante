6040db455ed93cf4ffd3ecf407e167d54517bb46
wizard-kgalm <cameron43@comcast.net>
2013-07-12 14:55:25 -0700

    Create logmarker.php

---
+++ scripts/logmarker.php
@@ -0,0 +1,13 @@
+<?php
+// DANTE SCRIPT ======
+// Name: Log Marker
+// Description: Drops a marker into the logs.
+// Author: SubjectX52873M
+// Version: 1.0
+// Priv: 99
+// Help: No options for this command.
+// ====================
+$dAmn->say( "Log marker set.", $c );
+registerChat( "(LOGMARKER)==========================", $c, FALSE );
+
+?>
