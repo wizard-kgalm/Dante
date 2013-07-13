a30149e4dfa0b4735c6f8b6888e57376cfe546dc
wizard-kgalm <cameron43@comcast.net>
2013-07-13 10:45:12 -0700

    Create rejoin.php

---
+++ modules/system/rejoin.php
@@ -0,0 +1,11 @@
+<?php
+if( $config['bot']['AutoRejoinWhenKicked'] ) {
+  $dAmn->joinRoom( $chatroom );
+}
+/*
+21:29:07 <SubjectX52873M> Don't compile, get a binary
+21:29:50 <ClanCC> can't run windows binary on linux without another program
+21:30:08 <SubjectX52873M> Linux bunary
+21:30:24 <SubjectX52873M> It's a type of pastry
+*/
+?>
